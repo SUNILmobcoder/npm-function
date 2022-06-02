2d9cebe7141aa9c2a8c4fc2d64ceca8f30362218
Sunil Kumar Yadav <mobcoderid-105@Mobcoders-Mac-mini-5.local>
2022-06-02 14:58:09 +0530

    Update: handle min-max validation

--- src/_validation/userResponseValidate.ts
+++ src/_validation/userResponseValidate.ts
@@ -5,11 +5,16 @@ const isEmpty = (str: string) => {
   return false;
 };
 
+// (min-max) => TextField, MultiLineText, Numeric, Date, DateTime, Phone, Currency, MultiSelect
+// (pattern) => TextField, MultiLineText
 const handleValidate = (props: QuestionProps, userResponse: string[]) => {
   let { Validation, Required, Type } = props;
 
   // if script have validation
   if (Validation) {
+    // if user haven't responsed
+    if (userResponse === null) return "This field can't be empty";
+
     // handle for text and multiline field
     if (Type === 'TextField' || Type === 'MultiLineText') {
       // handle validation if it have pattern
@@ -18,39 +23,135 @@ const handleValidate = (props: QuestionProps, userResponse: string[]) => {
         let msg = Validation.pattern.error;
         let pattern = new RegExp(regex);
 
-        // if user haven't responsed
-        if (userResponse === null) return msg;
-
         // if validation didn't match
         if (!pattern.test(userResponse[0])) return msg;
       }
 
-      // handle validation if it hav min-max
-      if (Validation.max && Validation.min) {
-        let min = parseInt(Validation.min.value);
-        let minMsg = Validation.min.error;
-
+      // handle validation if it have max
+      if (Validation.max) {
         let max = parseInt(Validation.max.value);
         let maxMsg = Validation.max.error;
-
-        // if user haven't responsed
-        if (userResponse === null) return minMsg;
-
-        if (userResponse[0].length < min) {
-          return minMsg;
-        }
         if (userResponse[0].length > max) {
           return maxMsg;
         }
       }
+
+      // handle validation if it have min
+      if (Validation.min) {
+        let min = parseInt(Validation.min.value);
+        let minMsg = Validation.min.error;
+        if (userResponse[0].length < min) {
+          return minMsg;
+        }
+      }
     }
 
-    // handle for Numeric and Phone field
-    if (Type === 'Numeric' || Type === 'Phone' || Type === 'Currency') {
+    // handle for Numeric field
+    if (Type === 'Numeric') {
+      // handle validation if it have max
+      if (Validation.max) {
+        let max = parseInt(Validation.max.value);
+        let maxMsg = Validation.max.error;
+        if (parseInt(userResponse[0]) > max) {
+          return maxMsg;
+        }
+      }
+
+      // handle validation if it have min
+      if (Validation.min) {
+        let min = parseInt(Validation.min.value);
+        let minMsg = Validation.min.error;
+        if (parseInt(userResponse[0]) < min) {
+          return minMsg;
+        }
+      }
     }
 
-    // handle for SingleSelect and MultiSelect field
-    if (Type === 'MultiSelect' || Type === 'SingleSelect') {
+    // handle date and dateTime field
+    if (Type === 'Date' || Type === 'DateTime') {
+      // handle validation if it have max
+      if (Validation.max) {
+        let max = Validation.max.value;
+        let maxMsg = Validation.max.error;
+        if (new Date(userResponse[0]) > new Date(max)) {
+          return maxMsg;
+        }
+      }
+
+      // handle validation if it have min
+      if (Validation.min) {
+        let min = Validation.min.value;
+        let minMsg = Validation.min.error;
+        if (new Date(userResponse[0]) < new Date(min)) {
+          return minMsg;
+        }
+      }
+    }
+
+    // handle for Phone field
+    if (Type === 'Phone') {
+      // handle validation if it have max
+      if (Validation.max) {
+        let max = parseInt(Validation.max.value);
+        let maxMsg = Validation.max.error;
+        if (userResponse[0].length > max) {
+          return maxMsg;
+        }
+      }
+
+      // handle validation if it have min
+      if (Validation.min) {
+        let min = parseInt(Validation.min.value);
+        let minMsg = Validation.min.error;
+        if (userResponse[0].length < min) {
+          return minMsg;
+        }
+      }
+    }
+
+    // handle for Currency field
+    if (Type === 'Currency') {
+      // handle validation if it have max
+      if (Validation.max) {
+        let max = parseInt(Validation.max.value);
+        let maxMsg = Validation.max.error;
+        if (parseInt(userResponse[0]) > max) {
+          return maxMsg;
+        }
+      }
+
+      // handle validation if it have min
+      if (Validation.min) {
+        let min = parseInt(Validation.min.value);
+        let minMsg = Validation.min.error;
+        if (parseInt(userResponse[0]) < min) {
+          return minMsg;
+        }
+      }
+    }
+
+    // handle for SingleSelect field
+    if (Type === 'SingleSelect') {
+    }
+    // handle for MultiSelect field
+    if (Type === 'MultiSelect') {
+      // handle validation if it have max
+      if (Validation.max) {
+        let max = parseInt(Validation.max.value);
+        let maxMsg = Validation.max.error;
+        if (userResponse.length > max) {
+          return maxMsg;
+        }
+      }
+
+      // handle validation if it have min
+      if (Validation.min) {
+        let min = parseInt(Validation.min.value);
+        let minMsg = Validation.min.error;
+        if (userResponse.length < min) {
+          return minMsg;
+        }
+      }
     }
   }
 
